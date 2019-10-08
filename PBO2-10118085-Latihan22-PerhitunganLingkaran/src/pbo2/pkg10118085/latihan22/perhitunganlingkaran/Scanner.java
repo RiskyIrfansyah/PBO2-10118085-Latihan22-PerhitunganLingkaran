e9761b2296a141299e5fc76216e0f5578b7d84df
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-10-08 21:46:29 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan22-PerhitunganLingkaran/src/pbo2/pkg10118085/latihan22/perhitunganlingkaran/Scanner.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan22.perhitunganlingkaran;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author User
+ */
+class Scanner {
+
+    Scanner(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean hasNextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int nextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
